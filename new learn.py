cf615c579c7bd92231d06cecc120e6722826c3bc
nikita17052001 <nikita.loskutov2001@gmail.com>
2024-09-24 22:12:26 +0300

    Add files via upload

---
+++ new learn.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hour_count = 1.5
+name_of_course = "Python"
+time_for_one_lesson = hour_count/homework_count
+print ("Курс:",name_of_course,"всего задач:",homework_count,  "затрачено часов:",hour_count,"среднее время выполнения:", time_for_one_lesson)
